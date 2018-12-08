5c659ebcbe03aff762d819f4dde878547725d0ec
LeeSeokCheol <45706732+LeeSeokCheol@users.noreply.github.com>
2018-12-08 17:40:15 +0900

    Add files via upload

---
+++ Assignment/empty.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 17:36:16 2018
+
+  
+x = 4
+y = 5
+z = x + y
+print("Output #2: Four plus five equals {0:d}.".format(z))
+            
+     # 두 리스트를 더한다.
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+c = a + b
+print("Output #3: {0}, {1}, {2}".format(a, b, c))
+       
+      
+x=9
+print("Output #4: {0}".format(x))
+print("Output #5: {0}".format(3**4))
+print("Output #6: {0}".format(int(8.3)/int(2.7)))
+print("OUtput #7: {0:.3f}".format(8.3/2.7))
+y = 2.5*4.8
+print("Output #8: {0:.1f}".format(y))
+r = 8/float(3)
+print("Output #9: {0:.2f}".format(r))
+print("Output #10: {0:.4f}".format(8.0/3))
