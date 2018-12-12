f81c3854b3c961c8b326ac8ff13896c1a22dfbfe
LeeSeokCheol <45706732+LeeSeokCheol@users.noreply.github.com>
2018-12-13 03:23:59 +0900

    Update day_month_year_datatime_code.py

--- day_month_year_datatime_code.py
+++ day_month_year_datatime_code.py
@@ -4,7 +4,7 @@ from math import exp, log, sqrt
 import re
 from datetime import date, time, datetime, timedelta
 
-#오늘 날짜와 년, 월, 일 요소들을 출력하기
+#Displaying today's date and year, month, and day elements.
 today = date.today()
 print("Output #41: today: {0!s}".format(today))
 print("Output #42: {0!s}".format(today.year))
@@ -13,36 +13,38 @@ print("Output #44: {0!s}".format(today.day))
 current_datetime = datetime.today()
 print("Output #45: {0!s}".format(current_datetime))
 
-#timedelta 함수를 이용하여 새로운 날짜 계산하기
+#Using timedelta to calculate new dates.
 one_day = timedelta(days=-1)
 yesterday = today + one_day
 print("Output #46: yesterday: {0!s}".format(yesterday))
 eight_hours = timedelta(hours=-8)
 print("Output #47: {0!s} {1!s}".format(eight_hours.days, eight_hours.seconds))
 
-#두 날짜 사이의 날짜 차이를 계산하기
-#str 함수는 결과를 하나의 문자열로 변환한다.
-#split 함수는 문자열을 공백문자에 따라 나누고, 나뉜 각 하위 문자열들을 하나늬 리스트에 넣는다.
+# Calculate the difference between two dates.
+#The str function converts the result to a single string.
+#The split function divides a string by whitespace, and puts each split substring into a list.
 date_diff = today - yesterday
 print("Output #48: {0!s}".format(date_diff))
 print("Output #49: {0!s}".format(str(date_diff).split()[0]))
 
-# date 객체를 특정 형식의 문자열으로 만들기
+# Making a date object a specific type of string.
 print("Output #50: {:s}".format(today.strftime('%m/%d/%y')))
 print("Output #51: {:s}".format(today.strftime('%b %d, %y')))
 print("Output #52: {:s}".format(today.strftime('%Y-%m-%d')))
 print("Output #53: {:s}".format(today.strftime('%B %d, %Y')))
       
-#날짜 문자열로 부터 특정 형식의 datetime 객체를 생성하기
+# Create a specific type of datetime object from a date string.
 date1 = today.strftime('%m/%d/%Y')
 date2 = today.strftime('%b %d, %Y')
 date3 = today.strftime('%Y-%m-%d')
 date4 = today.strftime('%B %d, %Y')
 
-#다른 date 형식을 지닌 4가지 문자열에 기반한
-# 각각 2종류의 datetime 객체들과 date 객체들
+# Based on 4 strings with different date formats
+# Each of the two datetime and date objects
 print("Output #54: {!s}".format(datetime.strptime(date1, '%m/%d/%Y')))
 print("Output #55: {!s}".format(datetime.strptime(date2, '%b %d, %Y')))
-# 날짜 부분만 출력하기
+# Print only date part
 print("Output #56: {!s}".format(datetime.date(datetime.strptime(date3, '%Y-%m-%d'))))
 print("Output #57: {!s}".format(datetime.date(datetime.strptime(date4, '%B %d, %Y'))))
+
+#References by Foudations for Analytics with Python.
